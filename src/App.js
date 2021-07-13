005be56dbb01c37b080a339ac9c204b7d25bc0ae
abecerraguz <alejandro.becerra@publimetro.cl>
2021-07-12 22:00:03 -0400

    Se gurada first-component

--- src/App.js
+++ src/App.js
@@ -2,48 +2,63 @@ import './App.css';
 import PersonCard from './components/PersonCard';
 
 function App() {
+
+  
   const cards = [
     {
+      id:1,
       firstName: "Doe",
       lastName: 'Jane',
       age:45,
       hairColor:'Black'
     },
     {
+      id:2,
       firstName: "Smith",
       lastName: 'Jhone',
       age:88,
       hairColor:'Brown'
     },
     {
+      id:3,
       firstName: "Fillmore",
       lastName: 'Millard',
       age:50,
       hairColor:'Brown'
     },
     {
+      id:4,
       firstName: "Smith",
       lastName: 'María',
       age:62,
       hairColor:'Green'
     }
   ]
+
+  let uniqid = require('uniqid');
+  console.log('uniqid-->', uniqid())
+
   return (
-    <div className="container">
-      {/* <div className="cards"> */}
-          {
-            cards.map(card => (
-              <PersonCard
-              firstName={card.firstName}
-              lastName={card.lastName}
-              age={card.age}
-              hairColor={card.hairColor}
-              >
-              </PersonCard>
-            ))
-          }
-        </div>
-    // </div> 
+  
+   
+        <ul className="container">
+            {
+              cards.map((cards)=> (
+              
+                <li key={uniqid()}>
+                  <PersonCard
+                  firstName={cards.firstName}
+                  lastName={cards.lastName}
+                  age={cards.age}
+                  hairColor={cards.hairColor}
+                  >
+                  </PersonCard>
+                </li>
+              ))
+            }
+        </ul>
+     
+ 
   );
 }
 
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const PersonCard = ({ firstName, lastName, age, hairColor }) => (
+class PersonCard extends Component {
+
+state = {
+    number: this.props.age,
+    firstName: this.props.firstName,
+    lastName: this.props.lastName,
+    age: this.props.age,
+    hairColor: this.props.hairColor,
+    id: this.props.id
+}
+
+  handleIncrement = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  handleDecrement = () => {
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+
+  handleReset = () => {
+    this.setState({
+      number: 0
+    })
+  }
+
+  render() {
+   
+    return (
+    <>
     <div className="card">
-        <h2 className="card__title">{firstName}, {lastName}</h2>
-        <p className="card__description">Age: {age}</p>
-        <p className="card__description">Air Color: {hairColor}</p>
+        <h2 className="card__title">{this.props.firstName}, {this.props.lastName}</h2>
+        <p className="card__description">Age: {this.state.number}</p>
+        <p className="card__description">Air Color: {this.props.hairColor}</p>
+        <button className="btn" onClick={this.handleIncrement}>Increment</button>
+        <button className="btn" onClick={this.handleDecrement}>Decrement</button>
+        <button className="btn" onClick={this.handleReset}>Reset</button>
     </div>
-);
+    </>
+    );
+  }
+}
+
 
 export default PersonCard;
